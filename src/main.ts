97d1725cbb10f7ebe327d3f6c50cd147eae5fb8a
Pigeonnie <1363746469@qq.com>
2025-07-30 13:57:34 +0800

    修改语法错误

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'element-plus/dist/index.css'
 import './assets/main.css'
-import '@/styles/fonts.css'
+import '@/styles/font.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
